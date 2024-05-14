4f0a82c6e372164bc28d8367af80737383217d0a
userNesh <134700020+userNesh@users.noreply.github.com>
2024-05-14 12:53:41 -0700

    Update and rename fe.unsuccessful.login.check.email.format.spec.ts to fe.unsuccessful.login.email.not.matched.spec.ts
    Update and Rename test

--- src/FE/tests/fe.unsuccessful.login.email.not.matched.spec.ts
+++ src/FE/tests/fe.unsuccessful.login.email.not.matched.spec.ts
@@ -7,6 +7,9 @@ test('should show message: email not valid', async function({page}) {
     await page.waitForTimeout(500)
     await page.locator('[id="email"]').fill('invalid@email')
 
+    await page.waitForTimeout(500)
+    await page.locator('[id="password"]').fill(process.env.LOGIN_PASSWORD!)
+    
     await page.waitForTimeout(1000)   
     await page.locator('[type="Submit"]').click()
 
